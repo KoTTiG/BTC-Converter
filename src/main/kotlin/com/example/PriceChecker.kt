fbbb68f6881b989fff68eecccad717c870b6be01
KoTTiG <d.gasilov@mail.ru>
2022-12-04 19:43:01 +0300

    #5 converter-4 Add errors handling

--- src/main/kotlin/com/example/PriceChecker.kt
+++ src/main/kotlin/com/example/PriceChecker.kt
@@ -5,32 +5,44 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.json.JSONObject
 
+import kotlin.NullPointerException
+
 
 private val COINBASE_API_URL = "https://api.coindesk.com/v1/bpi/currentprice/BTC.json"
 
+@Throws(NullPointerException::class)
 fun btcPriceCall(): Double {
-    val client = OkHttpClient()
-    val httpUrl = COINBASE_API_URL.toHttpUrl()
-    val request = Request.Builder().url(httpUrl).build()
-    val response = client.newCall(request).execute()
-    val body = response.body?.string()
-    val json = JSONObject(body)
-    val price = json.getJSONObject("bpi")
-        .getJSONObject("USD")
-        .getString("rate").replace(",", "").toDouble()
-    return price
+    try {
+        val client = OkHttpClient()
+        val httpUrl = COINBASE_API_URL.toHttpUrl()
+        val request = Request.Builder().url(httpUrl).build()
+        val response = client.newCall(request).execute()
+        val body = response.body!!.string()
+        val json = JSONObject(body)
+        val price = json.getJSONObject("bpi")
+            .getJSONObject("USD")
+            .getString("rate").replace(",", "").toDouble()
+        return price
+    } catch (e: NullPointerException) {
+        throw NullPointerException()
+    }
 }
 
+@Throws(NullPointerException::class)
 fun usdPriceCall(): Double {
-    val client = OkHttpClient()
-    val httpUrl = COINBASE_API_URL.toHttpUrl()
-    val request = Request.Builder().url(httpUrl).build()
-    val response = client.newCall(request).execute()
-    val body = response.body?.string()
-    val json = JSONObject(body)
-    val price = json.getJSONObject("bpi")
-        .getJSONObject("USD")
-        .getString("rate").replace(",", "").toDouble()
+    try {
+        val client = OkHttpClient()
+        val httpUrl = COINBASE_API_URL.toHttpUrl()
+        val request = Request.Builder().url(httpUrl).build()
+        val response = client.newCall(request).execute()
+        val body = response.body?.string()
+        val json = JSONObject(body)
+        val price = json.getJSONObject("bpi")
+            .getJSONObject("USD")
+            .getString("rate").replace(",", "").toDouble()
 
-    return (1.0 / price)
+        return (1.0 / price)
+    } catch (e: NullPointerException) {
+        throw NullPointerException()
+    }
 }
