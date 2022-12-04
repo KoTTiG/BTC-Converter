060abdd400bb35d7ed7e5e18bb7bea44e5a43105
KoTTiG <d.gasilov@mail.ru>
2022-12-04 19:10:41 +0300

    #3 converter-3 created a two-side converter with output

--- src/main/kotlin/com/example/PriceChecker.kt
+++ src/main/kotlin/com/example/PriceChecker.kt
@@ -8,7 +8,7 @@ import org.json.JSONObject
 
 private val COINBASE_API_URL = "https://api.coindesk.com/v1/bpi/currentprice/BTC.json"
 
-fun priceCall(): String? {
+fun btcPriceCall(): Double {
     val client = OkHttpClient()
     val httpUrl = COINBASE_API_URL.toHttpUrl()
     val request = Request.Builder().url(httpUrl).build()
@@ -17,7 +17,20 @@ fun priceCall(): String? {
     val json = JSONObject(body)
     val price = json.getJSONObject("bpi")
         .getJSONObject("USD")
-        .getString("rate")
+        .getString("rate").replace(",", "").toDouble()
     return price
+}
 
+fun usdPriceCall(): Double {
+    val client = OkHttpClient()
+    val httpUrl = COINBASE_API_URL.toHttpUrl()
+    val request = Request.Builder().url(httpUrl).build()
+    val response = client.newCall(request).execute()
+    val body = response.body?.string()
+    val json = JSONObject(body)
+    val price = json.getJSONObject("bpi")
+        .getJSONObject("USD")
+        .getString("rate").replace(",", "").toDouble()
+
+    return (1.0 / price)
 }
